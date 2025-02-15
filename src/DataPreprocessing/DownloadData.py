3107682b8a81b160821455f5e5e49a550fe1f426
Murilo Pescador <murilofpc@gmail.com>
2025-02-15 10:50:58 -0300

    Create output directory for downloaded data in DownloadData.py

--- src/DataPreprocessing/DownloadData.py
+++ src/DataPreprocessing/DownloadData.py
@@ -14,6 +14,8 @@ def DownloadDataFromGoogleDrive(logger=Log):
     output_path = os.path.join(script_dir,'Data')
     ouuput_path_file = os.path.join(output_path,'Data.zip')
 
+    os.path.makedirs(output_path, exist_ok=True)
+
     if os.path.exists(ouuput_path_file):
         logger.info("Zip já existe, retornando")
         return
